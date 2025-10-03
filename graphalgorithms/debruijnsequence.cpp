7ec0f817a64b32f6f374ccb866e52c49cc2d1e5e
Noah Hessen Bjerke <noahhessenbjerke@dhcp-10-22-106-64.wlan.ntnu.no>
2025-10-03 12:11:13 +0200

    more graph

--- graphalgorithms/debruijnsequence.cpp
+++ graphalgorithms/debruijnsequence.cpp
@@ -7,6 +7,34 @@ typedef vector<vll> vvll;
 
 int main(){
     cin.tie(0)->sync_with_stdio(0);
-    ll n,m;cin>>n>>m;
-    
+    ll n;cin>>n;
+
+    if(n==1){
+        cout<<"01\n";
+        return 0;
+    }
+
+    ll M = 1 << (n-1);
+    ll mask = M-1;
+    vll it(M,0);
+    string seq;
+
+    stack<ll> st;
+    st.push(0);
+    while(!st.empty()){
+        ll u = st.top();
+        ll& i = it[u];
+        if(i<2){
+            ll b = i++;
+            ll v = (u<<1 & mask) | b;
+            st.push(v);
+        }
+        else{
+            st.pop();
+            if(!st.empty())seq.push_back(char('0'+(u&1)));
+        }
+    }
+    reverse(seq.begin(), seq.end());
+    cout<<string(n-1,'0')<<seq<<"\n";
+    return 0;
 }
