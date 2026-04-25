62c3f79a88783f17b7bfa960001116bccda0f747
Noah Hessen Bjerke <noahhb13@gmail.com>
2026-04-25 12:36:57 +0300

    sliding window xor

---
+++ slidingwindowproblems/slidingwindowxor.cpp
@@ -0,0 +1,28 @@
+#include"bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+ 
+int main()
+{
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+    
+    ll n,k;cin>>n>>k;
+    ll x,a,b,c;cin>>x>>a>>b>>c;
+    
+    ll sm = 0;
+    ll first = x;
+    for(ll i = 0 ; i < k ; ++i){
+        sm^=x;
+        x=(a*x+b)%c;
+    }
+    ll ans = sm;
+    for(ll i = k ; i<n ; ++i){
+        sm^=x;
+        sm^=first;
+        first = (first*a+b)%c;
+        x = (x*a+b)%c;
+        ans ^= sm;
+    }
+    cout << ans << '\n';
+}
